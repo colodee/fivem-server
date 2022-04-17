c231b26b1cbf43699490b2b88826fe5ab28e717f
colodee <92323973+colodee@users.noreply.github.com>
2022-04-18 00:11:40 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt install xz-utils -y
+
+mkdir -p /root/pma-4879/FXServer/server
+
+cd /root/pma-4879/FXServer/server
+
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/4879-4f12006546d1025efb34b1fe7de6e9958b3bb975/fx.tar.xz
+
+tar xf fx.tar.xz
+
